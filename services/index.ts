a11476e8b59579b94504449550301120fb632a1d
zzedddd <melegend.forever@protonmail.com>
2023-01-30 01:53:16 +0530

    fixed: ts errors

--- services/index.ts
+++ services/index.ts
@@ -1,6 +1,6 @@
 import { gql, request } from "graphql-request";
 
-const graphqlAPI = process.env.NEXT_PUBLIC_API_URL;
+const graphqlAPI: string = process.env.NEXT_PUBLIC_API_URL!;
 
 export const getPosts = async () => {
     const query = gql`
