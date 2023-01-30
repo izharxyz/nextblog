06438f2588c02e01a3a28915ddf2b4f80535a277
zzedddd <melegend.forever@protonmail.com>
2023-01-31 01:19:45 +0530

    added: dynamic category render and styles

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import Link from 'next/link';
+import { getCategories } from '$services';
 
 const Categories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    getCategories()
+      .then((newCategories) => setCategories(newCategories))
+  }, [])
+  
+
   return (
-    <div>Categories</div>
+    <div className='bg-white shadow-lg rounded-lg p-8 mb-8 pb-12'>
+      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+        Categories
+      </h3>
+      {categories.map((category) => (
+        <Link key={category.slug} href={`/category/${category.slug}`}>
+          <span className='cursor-pointer pb-3 mb-3 block'>
+            {category.name}
+          </span>
+        </Link>
+      ))}
+    </div>
   )
 }
 
