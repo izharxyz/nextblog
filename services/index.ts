df32b8e2484f1ce02a57be56ff0cc35f258cc964
zzedddd <melegend.forever@protonmail.com>
2023-01-30 21:56:47 +0530

    added: query for fetching recent 3 posts

--- services/index.ts
+++ services/index.ts
@@ -37,3 +37,24 @@ export const getPosts = async () => {
     const result = await request(graphqlAPI, query);
     return result.postsConnection.edges;
 }
+
+export const getRecentPosts = async () => {
+    const query = gql`
+    query GetRecentPostDetails {
+        posts (
+            orderBy: createdAt_ASC
+            last: 3
+        ) {
+            title
+            featuredImage {
+                url
+            }
+            createdAt
+            slug
+        }
+    }
+    `
+
+    const result = await request(graphqlAPI, query);
+    return result.posts;
+}
