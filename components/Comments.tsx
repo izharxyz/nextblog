c8761be82592b3b35773922a3d9df441d3ff2a9c
zzedddd <melegend.forever@protonmail.com>
2023-02-02 06:06:36 +0530

    added: comment component

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import parse from 'html-react-parser';
+
+import { getComments } from '$services';
 
 const Comments = ({ slug }: any) => {
+  const [comments, setComments] = useState<any>([]);
+
+  useEffect(() => {
+    getComments(slug)
+      .then((result) => setComments(result))
+  }, []) 
+  
+
   return (
-    <div>Comments</div>
+    <>
+      {comments.length > 0 && (
+        <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
+          <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+            {comments.length}
+            {' '}
+            Comments
+          </h3>
+          {comments.map((comment: any) => (
+            <div key={comment.createdAt} className='border-b border-gray-100 mb-4 pb-4'>
+              <p className='mb-4'>
+                <span className='font-semibold'>
+                  {comment.name}
+                  {' '}
+                  on 
+                  {' '}
+                  {moment(comment.createdAt).format('MMM DD, YY')}
+                </span>
+              </p>
+              <p className='whitespace-pre-line text-gray-600 w-full'>
+                {parse(comment.comment)}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   )
 }
 
